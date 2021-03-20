6955e053eb09bfce521732ef4d61608d5172a84d
Carlos Willian <carlodwillian@gmail.com>
2021-03-20 17:39:39 -0300

    Upload exercício

--- CursoJS/aula14ex/ex018/script.js
+++ CursoJS/aula14ex/ex018/script.js
@@ -1,1 +1,18 @@
-alert('Olá!')
+function gerar() {
+    var num = document.getElementById('numtxt')
+    var tab = document.getElementById('selTab')
+    var n = Number(num.value)
+    if (num.value.length == 0) {
+        window.alert('[ERRO] digite um número!')
+    } else {
+        var c = 1
+        tab.innerHTML = ''
+        while (c <= 10) { 
+            var item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
+        }
+    }
+}
